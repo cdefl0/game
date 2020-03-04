953e0d4ab012eadc84faf81cbca2cc962ba1b3fc
cdefl0 <60612704+cdefl0@users.noreply.github.com>
2020-03-04 08:48:23 -0600

    Add files via upload

--- game.py
+++ game.py
@@ -3,6 +3,11 @@ print("Welcome to my game! What is your name?")
 name = input
 int atk = 0
 int dex = 0
-int hp = 0
+int hp = 10
 print("Pick a value for strength (1-5)")
 atk = input
+print("Pick a value for dexterity (1-5)")
+dex = input
+print("Your health is 10, your strength is " + atk + " and your dexterity is " + dex + ".")
+print("You enter an abandoned house, and the door slams behind you. You are trapped.")
+print("An old man greets you.")
