faeaba256849fbc5526add022dd4414731520d9f
cdefl0 <60612704+cdefl0@users.noreply.github.com>
2020-03-09 08:48:18 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from random import randint
 string name = " "
 print("Welcome to my game! What is your name?")
 name = input
@@ -10,4 +11,12 @@ print("Pick a value for dexterity (1-5)")
 dex = input
 print("Your health is 10, your strength is " + atk + " and your dexterity is " + dex + ".")
 print("You enter an abandoned house, and the door slams behind you. You are trapped.")
-print("An old man greets you.")
+print("An old man greets you.  He seems harmless, but then pulls out a rusty old knife.")
+print("Enter 1 to attempt to flee him or enter 2 to attempt to fight him.")
+if input = 1:
+    print("He trips you and then stabs you to death.")
+if input = 2:
+    if (randint(0, 12) * atk) > 2:
+        print("You take his knife and stab him in the eye.  He keels over dead.")
+        
+s
